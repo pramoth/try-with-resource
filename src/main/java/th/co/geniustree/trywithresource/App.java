810ae4915b5289f5f0e853b4f31c47b87f2eb16f
pramoth <pramoth@geniustree.co.th>
2016-02-27 16:48:39 +0700

    update

--- src/main/java/th/co/geniustree/trywithresource/App.java
+++ src/main/java/th/co/geniustree/trywithresource/App.java
@@ -7,15 +7,15 @@ package th.co.geniustree.trywithresource;
 public class App {
     public static void main(String[] args) {
         try(MyAutoCloseable x = new MyAutoCloseable()){
-            System.out.println("(\"---------body"+x+"---------");
+            System.out.println("--------- body-"+x+"---------");
         }catch(Exception e){
-            System.out.println("---------catch---------");
+            System.out.println("--------- catch---------");
             System.out.println("---------"+e.getMessage());
             Throwable[] suppressed = e.getSuppressed();
             System.out.println("--------- suppressed lengh = "+suppressed.length);
-            System.out.println("---------end catch---------");
+            System.out.println("--------- end catch---------");
         }finally{
-            System.out.println("---------finally---------");
+            System.out.println("--------- finally---------");
         }
     }
 }
