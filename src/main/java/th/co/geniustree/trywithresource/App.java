34ef67cb726b09208321ad2669822c3dd74da2a7
pramoth <pramoth@geniustree.co.th>
2016-02-27 16:55:45 +0700

    update again.

--- src/main/java/th/co/geniustree/trywithresource/App.java
+++ src/main/java/th/co/geniustree/trywithresource/App.java
@@ -5,16 +5,19 @@ package th.co.geniustree.trywithresource;
  * @author pramoth
  */
 public class App {
+
     public static void main(String[] args) {
-        try(MyAutoCloseable x = new MyAutoCloseable()){
-            System.out.println("--------- body-"+x+"---------");
-        }catch(Exception e){
+        try (MyAutoCloseable x = new MyAutoCloseable()) {
+            System.out.println("--------- body-" + x + "---------");
+            x.mimicError();
+            //=============close was called=============
+        } catch (Exception e) {
             System.out.println("--------- catch---------");
-            System.out.println("---------"+e.getMessage());
+            System.out.println("---------" + e.getMessage());
             Throwable[] suppressed = e.getSuppressed();
-            System.out.println("--------- suppressed lengh = "+suppressed.length);
+            System.out.println("--------- suppressed lengh = " + suppressed[0].getMessage());
             System.out.println("--------- end catch---------");
-        }finally{
+        } finally {
             System.out.println("--------- finally---------");
         }
     }
--- src/main/java/th/co/geniustree/trywithresource/MyAutoCloseable.java
+++ src/main/java/th/co/geniustree/trywithresource/MyAutoCloseable.java
@@ -10,13 +10,16 @@ import java.io.IOException;
 public class MyAutoCloseable implements Closeable{
 
     public MyAutoCloseable() throws IOException {
-        throw new IOException("Exception while constructor.");
+        System.out.println("================constructor call=================");
+    }
+    public void mimicError() throws IOException{
+        throw new IOException("Exception while do task.");
     }
 
     @Override
     public void close()throws IOException{
         System.out.println("=============close was called=============");
-        //throw new IOException("Exception while close. Should see in suppress exception");
+        throw new IOException("Exception while close. Should see in suppress exception");
     }
     
 }
